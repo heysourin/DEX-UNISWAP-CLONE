27b5e36533245793cc77acb4385dae0fff5531b2
heysourin <sourin07@gmail.com>
2023-01-16 23:20:24 +0530

    Updated

--- blockchain/contracts/Exchange.sol
+++ blockchain/contracts/Exchange.sol
@@ -6,12 +6,48 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Exchange is ERC20 {
     address public cryptoDevTokenAddress;
 
-    // Exchange is inheriting ERC20, because our exchange would keep track of Crypto Dev LP tokens
+    //TODO: Exchange is inheriting ERC20, because our exchange would keep track of Crypto Dev LP tokens
     constructor(address _CryptoDevtoken) ERC20("CryptoDev LP Token", "CDLP") {
         require(
             _CryptoDevtoken != address(0),
             "Token address passed is a null address"
         );
         cryptoDevTokenAddress = _CryptoDevtoken;
+    }
+
+    //TODO: Getting balance of CryptoDev tokens in this CONTRACT.
+    function getReserve() public view returns (uint) {
+        return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+    }
+
+    //TODO: Lets create an 'addLiquidity' function which would add liquidity in the form of ETH and Crypto Dev tokens to our contract.
+
+    function addLiquidity(uint _amount) public payable returns (uint) {
+        uint liquidity;
+        uint ethBalance = address(this).balance;
+        uint cryptoDevTokenReserve = getReserve();
+        ERC20 cryptoDevToken = ERC20(cryptoDevTokenAddress);
+
+        if (cryptoDevTokenReserve == 0) {
+            cryptoDevToken.transferFrom(msg.sender, address(this), _amount);
+            liquidity = ethBalance;
+            _mint(msg.sender, liquidity);
+        } else {
+            uint ethReserve = ethBalance - msg.value;
+            uint cryptoDevTokenAmount = (msg.value * cryptoDevTokenReserve) /
+                (ethReserve);
+            require(
+                _amount >= cryptoDevTokenAmount,
+                "Amount of tokens sent is less than the minimum tokens required"
+            );
+            cryptoDevToken.transferFrom(
+                msg.sender,
+                address(this),
+                cryptoDevTokenAmount
+            );
+            liquidity = (totalSupply() * msg.value) / ethReserve;
+            _mint(msg.sender, liquidity);
+        }
+        return liquidity;
     }
 }
